908063c4a7602ca6a899f1bfefb1830c0cba5b81
vespertron <vesper.annstas@gmail.com>
2017-09-07 07:36:10 +0200

    Add styling and jQuery

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function() {
+  $(".fade1").fadeIn(1000);
+  $(".fade2").fadeIn(2000);
+  $(".fade3").fadeIn(3000);
+  $(".fade4").fadeIn(4000);
+  $(".fade5").fadeIn(5000);
+  $(".fade6").fadeIn(6000);
+  $(".fade7").fadeIn(7000);
+  $(".fade8").fadeIn(8000);
+  $(".fade9").fadeIn(9000);
+});
